eacbc2bf6c40eeb2ff0a61923f449dfe22e835f8
MaramFahad <maramll98ll@gmail.com>
2023-08-01 17:27:01 +0300

    lab solved

--- lembdaLab.py
+++ lembdaLab.py
@@ -1,43 +1,24 @@
 #Q1
-"""def vowels (sentence):
-     sentence=sentence.lower
-     vowel=["a","e","i","o","u"]
-     lst = []
-     count=0
-     for letter in sentence():
-         lst.append(letter)
-        
-     for n in lst:
-        if lst[n]==vowel:
-             count+=n
-        return count + vowels(sentence)
-vowels("Hello my")
-"""
-#Q2  DDDDOOOONNNNEEEE
+
+def vowel_counter(sentence):
+     if len(sentence)==0 or sentence==None:
+        return 0
+     if sentence[0].lower() in ['o','a','i','e','u']:
+        return 1 + vowel_counter(sentence[1:])
+     else:
+        return vowel_counter(sentence[1:])
+
+print(f"number of vowels is: {vowel_counter('I love python')}\n")
+
+
+#Q2  
+
 def subtract(n):
 	return n * n
+
 numbers = [40,35,10,15,20]
 print("App to multiplying each number in the list by itself ")
 print(f"Original list: {numbers}")
 
 result = map(subtract, numbers)
 print(f"List after multiplied: {list(result)}")
-
-
-"""def vowels (sentence):
-     sentence=sentence.lower
-     vowel=["a","e","i","o","u"]
-     lst = []
-     count=0
-     for letter in sentence():
-         lst.append(letter)
-         if vowel in lst:
-             count+=
-     return count + vowels(sentence)
-vowels("Hello my")
-
-"""
-sentence="my name is "
-vowels=["a","e","i","o","u"]
-vowel=sentence.find
-print(vowel)
