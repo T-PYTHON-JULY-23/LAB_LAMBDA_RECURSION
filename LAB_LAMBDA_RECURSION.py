436f40b08129f176a75343c45bdf0b16fab4ccb0
= <=>
2023-08-01 15:00:32 +0300

    LAB_LAMBDA_RECURSION

---
+++ LAB_LAMBDA_RECURSION.py
@@ -0,0 +1,38 @@
+## 1) Using recursion, if given a word/phrase return how many vowels(a,e,i,o,u) are in that phrase or word:
+
+#### Note: the function should be able to  count how many vowels no matter if it is lowercase or uppercase . 
+#### Example: given the phrase `I love python` , it should return : `4` 
+
+def count_vowels(phrase):
+    vowels="a,e,I,o,u"
+    if len(phrase)==0:
+        return 0
+    elif phrase[0] in vowels :
+        return 1 + count_vowels(phrase[1:])
+    else:
+        return count_vowels(phrase[1:])
+
+
+phrase = "I love python"
+vowel_count = count_vowels(phrase)
+print("the count of vowels in phrase", phrase, "is:", vowel_count)
+
+# 2) Given a list of numbers : `[40,35, 10, 15, 20]`
+
+#### create a new list containing each number from the previous list mutliplied by itself.
+#### print the new list.
+#### Hint: use map() with a lambda funciton
+
+MyList = [40,35, 10, 15, 20]
+mutliplied_list = list(map(lambda x : x*x,MyList))
+print(mutliplied_list) 
+
+
+
+
+ 
+    
+
+
+ 
+    
